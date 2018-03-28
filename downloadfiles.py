b34a61228d1d888c3633f6be35db5d1aae1eb8b9
Nihal Soans <nihalsoans91@gmail.com>
2018-03-28 17:57:32 -0400

    Added Python tool to Download the dataset
    The tool to download the data

---
+++ downloadfiles.py
@@ -0,0 +1,27 @@
+import requests
+import time
+
+start_time = time.time()
+print("Downloading files for training")
+i=0
+with open("train.txt",'r') as trainfile:
+    for line in trainfile:
+        line=line.rstrip()
+        print("Processing "+line+" as cilia"+str(i)+".tar")
+        url = "https://storage.googleapis.com/uga-dsp/project4/data/"+line+".tar"
+        r = requests.get(url)
+        with open("train/cilia"+str(i)+".tar", 'wb') as file:
+            file.write(r.content)
+        i+=1
+print("Downloading files for testing")
+i=0
+with open("test.txt",'r') as testfile:
+    for line in testfile:
+        line=line.rstrip()
+        print("Processing "+line+" as cilia"+str(i)+".tar")
+        url = "https://storage.googleapis.com/uga-dsp/project4/data/"+line+".tar"
+        r = requests.get(url)
+        with open("test/cilia"+str(i)+".tar", 'wb') as file:
+            file.write(r.content)
+        i+=1
+print("--- %s seconds ---" % (time.time() - start_time))
