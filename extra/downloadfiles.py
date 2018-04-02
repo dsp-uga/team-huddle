2ed5aeb3b657fe9791cd7ac252239ae8239f98bd
Nihal Soans <nihalsoans91@gmail.com>
2018-04-02 11:53:10 -0400

    Added the code to download masks

--- extra/downloadfiles.py
+++ extra/downloadfiles.py
@@ -13,6 +13,17 @@ with open("train.txt",'r') as trainfile:
         with open("train/cilia"+str(i)+".tar", 'wb') as file:
             file.write(r.content)
         i+=1
+print("Downloading masks")
+i=0
+with open("train.txt",'r') as trainfile:
+    for line in trainfile:
+        line=line.rstrip()
+        print("Processing "+line+" as cilia"+str(i)+".png")
+        url = "https://storage.googleapis.com/uga-dsp/project4/masks/"+line+".png"
+        r = requests.get(url)
+        with open("train/mask/cilia"+str(i)+".png", 'wb') as file:
+            file.write(r.content)
+        i+=1
 print("Downloading files for testing")
 i=0
 with open("test.txt",'r') as testfile:
